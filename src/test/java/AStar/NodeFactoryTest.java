456227a0e09af51970774b9fc1010735de7bfea2
L4B0MB4 <bommersbach1996@gmail.com>
2019-01-01 22:04:48 +0100

    formated

--- src/test/java/AStar/NodeFactoryTest.java
+++ src/test/java/AStar/NodeFactoryTest.java
@@ -11,10 +11,11 @@ import java.util.ArrayList;
  * Unit test for simple App.
  */
 public class NodeFactoryTest {
-    /**
-     * Rigorous Test.
-     */
+
     @Test
+    /**
+     * Tests if a node can be created properly in the NodeFactory
+     */
     public void canCreateNode() {
         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
         ArrayList<Integer> aList = new ArrayList<>();
@@ -22,9 +23,13 @@ public class NodeFactoryTest {
         list.add(aList);
         ArrayList<Node> nodes = NodeFactory.createNodes(list);
         assertEquals(nodes.size(), 1);
+        assertTrue(nodes.get(0).getWeight() ==3);
     }
 
     @Test
+    /**
+     * Tests if empty node lists can be handled by the NodeFactory
+     */
     public void emptyNodeList() {
         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
         ArrayList<Node> nodes = NodeFactory.createNodes(list);
@@ -32,6 +37,9 @@ public class NodeFactoryTest {
     }
 
     @Test
+    /**
+     * Tests if the Nodefactory sets the distance of the nodes properly
+     */
     public void setDistance() {
         Node n = new Node(new Point(1, 1), 1);
         ArrayList<Node> list = new ArrayList<>();
