cf216470aea847b185149501eb2e5dbe1ae71552
L4B0MB4 <metalfan65@gmail.com>
2019-01-06 17:40:18 +0100

    added factor for distance

--- src/main/java/AStar/NodeFactory.java
+++ src/main/java/AStar/NodeFactory.java
@@ -5,8 +5,11 @@ import java.util.ArrayList;
 
 public class NodeFactory {
 
+    public static final int HEURSITIC_FACTOR = 4;
+
     /**
      * Creates a arraylist of nodes out of a two dimensional arraylist of integers
+     * 
      * @param dataset two dimensional arraylist of integers
      * @return arraylist of nodes
      */
@@ -14,17 +17,28 @@ public class NodeFactory {
         ArrayList<Node> nodes = new ArrayList<>();
         for (int y = 0; y < dataset.size(); y++) {
             for (int x = 0; x < dataset.get(y).size(); x++) {
-                int value=Integer.MAX_VALUE;
-                switch(dataset.get(y).get(x))
-                {
-                    case 0: value =4;break;
-                    case 1:value =7; break;
-                    case 2:value =2; break;
-                    case 3:value =5; break;
-                    case 4:value =3; break;
-                    case 5:value =17; break;
+                int value = Integer.MAX_VALUE;
+                switch (dataset.get(y).get(x)) {
+                case 0:
+                    value = 4;
+                    break;
+                case 1:
+                    value = 7;
+                    break;
+                case 2:
+                    value = 2;
+                    break;
+                case 3:
+                    value = 5;
+                    break;
+                case 4:
+                    value = 3;
+                    break;
+                case 5:
+                    value = 17;
+                    break;
                 }
-                Node n = new Node(new Point(x, y),value);
+                Node n = new Node(new Point(x, y), value);
                 nodes.add(n);
             }
         }
@@ -32,9 +46,11 @@ public class NodeFactory {
     }
 
     /**
-     * This function calculates and sets the distance between every node in a list and a specific point. Distance is calulated by distance^2 =x^2 + y^2 
+     * This function calculates and sets the distance between every node in a list
+     * and a specific point. Distance is calulated by distance^2 =x^2 + y^2
+     * 
      * @param nodes list of nodes that will get a distance
-     * @param end end point / goal 
+     * @param end   end point / goal
      */
     public static void setDistance(ArrayList<Node> nodes, Point end) {
         for (int i = 0; i < nodes.size(); i++) {
@@ -42,7 +58,7 @@ public class NodeFactory {
             int xdist = end.x - n.getPosition().x;
             int ydist = end.y - n.getPosition().y;
             double distance = Math.sqrt(Math.pow(xdist, 2) + (Math.pow(ydist, 2)));
-            nodes.get(i).setDistance(distance);
+            nodes.get(i).setDistance(distance * HEURSITIC_FACTOR);
         }
     }
 }
